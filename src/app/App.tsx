51ceba0c7e35d1bcf1868759515c5e5ce030f93c
annaSemenova <anna.7nova@yandex.by>
2025-08-21 15:58:24 +0300

    feat(rouring, Login): add routing and react-hook-form for validation form

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import { getTheme } from '../common/theme/theme'
-import { Main } from './Main'
 import styles from './App.module.css'
 import { ErrorSnackBar, Header } from '@/common/components'
 import { useAppSelector } from '@/common/hooks'
 import { selectTheme } from './app-slice'
+import { Routing } from '@/common/routing'
 
 export function App() {
   const themeMood = useAppSelector(selectTheme)
@@ -13,7 +13,7 @@ export function App() {
     <div className={styles.app}>
       <ThemeProvider theme={getTheme(themeMood)}>
         <Header />
-        <Main />
+        <Routing/>
         <ErrorSnackBar/>
         <CssBaseline />
       </ThemeProvider>
